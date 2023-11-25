3bb918c5d51173e2eeae88fa321e3341e90a2700
Beesan <beesan.demaide@gmail.com>
2023-11-25 04:16:55 +0200

    gui

--- src/P2PChatAppUDP.java
+++ src/P2PChatAppUDP.java
@@ -1,4 +1,18 @@
+import javax.swing.*;
+
 public class P2PChatAppUDP {
+    private JTextField text_Local_IP;
+    private JTextField text_Local_port;
+    private JTextField text_Remote_port;
+    private JTextField text_remote_ip;
+    private JButton deleteIndexButton;
+    private JTextArea textArea_Send;
+    private JTextField status_text;
+    private JTextArea textArea_server;
+    private JButton deleteAllButton;
+    private JButton sendButton;
+    private JTextField textField1;
+
     public static void main(String[] args){
 
 
